df9ee56a9c6283057651204a01fc42a41d37b8e3
Wren Sharp <admin@worldanchor.com>
2021-08-22 20:31:24 -0500

    start of refactoring to develop forge and fabric mods

--- BuildToDevServer.bat
+++ BuildToDevServer.bat
@@ -1,8 +1,8 @@
 set modname=%1
 set modfile=%2
 
-del C:\Users\erik\Desktop\Github\fabric-server\mods\%modname%*.jar
-copy %~dp0\%modname%\build\libs\%modfile% C:\Users\erik\Desktop\Github\fabric-server\mods\%modfile%
-git -C C:\Users\erik\Desktop\Github\fabric-server add . 
-git -C C:\Users\erik\Desktop\Github\fabric-server\ commit -m "build commit"
-git -C C:\Users\erik\Desktop\Github\fabric-server\ push git@github.com:TheWorldAnchor/fabric-server.git dev
+del C:\Users\erik\Desktop\Github\minecraft-server\mods\%modname%*.jar
+copy %~dp0\%modname%\build\libs\%modfile% C:\Users\erik\Desktop\Github\minecraft-server\mods\%modfile%
+git -C C:\Users\erik\Desktop\Github\minecraft-server add . 
+git -C C:\Users\erik\Desktop\Github\minecraft-server\ commit -m "build commit"
+git -C C:\Users\erik\Desktop\Github\minecraft-server\ push git@github.com:TheWorldAnchor/minecraft-server.git dev
