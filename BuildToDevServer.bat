545bf66d1b7d8fded065105f6209cb77c879833d
Wren Sharp <admin@worldanchor.com>
2021-08-22 21:46:13 -0500

    still working on adding forge support

--- BuildToDevServer.bat
+++ BuildToDevServer.bat
@@ -1,8 +1,20 @@
 set modname=%1
 set modfile=%2
 
-del C:\Users\erik\Desktop\Github\minecraft-server\mods\%modname%*.jar
-copy %~dp0\%modname%\build\libs\%modfile% C:\Users\erik\Desktop\Github\minecraft-server\mods\%modfile%
+if not x%modfile:-fabric-mc=%==x%modfile% GOTO FABRIC
+if not x%modfile:-forge-mc=%==x%modfile% GOTO FORGE
+:AFTER
 git -C C:\Users\erik\Desktop\Github\minecraft-server add . 
 git -C C:\Users\erik\Desktop\Github\minecraft-server\ commit -m "build commit"
 git -C C:\Users\erik\Desktop\Github\minecraft-server\ push git@github.com:TheWorldAnchor/minecraft-server.git dev
+EXIT
+
+:FABRIC
+del C:\Users\erik\Desktop\Github\minecraft-server\mods\fabric\%modname%*.jar
+copy %~dp0fabric\%modname%\build\libs\%modfile% C:\Users\erik\Desktop\Github\minecraft-server\mods\fabric\%modfile%
+GOTO AFTER
+
+:FORGE
+del C:\Users\erik\Desktop\Github\minecraft-server\mods\forge\%modname%*.jar
+copy %~dp0forge\%modname%\build\libs\%modfile% C:\Users\erik\Desktop\Github\minecraft-server\mods\forge\%modfile%
+GOTO AFTER
--- GetStructuresFromBuildServer.bat
+++ GetStructuresFromBuildServer.bat
@@ -1,2 +1,2 @@
-del /f C:\Users\erik\Desktop\Github\fabric-mods\world-anchor-structures\src\main\resources\data\world-anchor-structures\structures\*
-scp -rpP 958 erik@play.worldanchor.com:/opt/minecraft/build/world/generated/world-anchor-structures/structures/* C:\Users\erik\Desktop\Github\fabric-mods\world-anchor-structures\src\main\resources\data\world-anchor-structures\structures
+del /f C:\Users\erik\Desktop\Github\minecraft-mods\fabric\world-anchor-structures\src\main\resources\data\world-anchor-structures\structures\*
+scp -rpP 958 erik@play.worldanchor.com:/opt/minecraft/build/world/generated/world-anchor-structures/structures/* C:\Users\erik\Desktop\Github\minecraft-mods\fabric\world-anchor-structures\src\main\resources\data\world-anchor-structures\structures
