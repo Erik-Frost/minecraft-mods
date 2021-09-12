532ba753046137f9a0cb6e83c32c3e87dc956f66
Wren Sharp <admin@worldanchor.com>
2021-09-11 22:26:08 -0500

    Created crippling debt mod

---
+++ fabric/world-anchor-crippling-debt/src/main/java/com/worldanchor/cripplingdebt/Server.java
@@ -0,0 +1,17 @@
+package com.worldanchor.cripplingdebt;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class Server implements DedicatedServerModInitializer {
+    public static final String MODID = "world-anchor-crippling-debt";
+
+    @Override
+    public void onInitializeServer() {
+
+    }
+
+
+
+}
+
+
